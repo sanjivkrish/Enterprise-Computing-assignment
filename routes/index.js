e31295b5eef036c9796bf3ba7dd546ac48c5c044
Sanjiv Krish <sanjiv436.k@gmail.com>
2017-11-10 12:49:41 +0100

    server: routes: pizza: Include "updatePizzaById" operation
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -29,14 +29,14 @@ router.post('/pizza', (req, res) => {
           // Duplicate ID
           if (pizzaList[i].id == pizza.id) {
             isValidPizza = false;
-            res.status(401).send('Pizza with same ID already exists');
+            res.status(400).send('Pizza with same ID already exists');
             break;
           }
 
           // Duplicate Name
           if (pizzaList[i].name == pizza.name) {
             isValidPizza = false;
-            res.status(401).send('Pizza with same Name already exists');
+            res.status(400).send('Pizza with same Name already exists');
             break;
           }
         }
@@ -82,6 +82,43 @@ router.get('/pizza/:pizzaId', (req, res) => {
   if (isPizzaFound) {
     // Pizza found
     res.status(200).send(pizzaList[index]);
+  } else {
+    // No pizza found
+    res.status(404).send('Pizza could not be found');
+  }
+});
+
+//
+// Update pizza by ID
+//
+router.put('/pizza/:pizzaId', (req, res) => {
+  var isPizzaFound = false;
+  var index = 0;
+
+  for (index = 0; index < pizzaList.length; index++) {
+    if (pizzaList[index].id == req.params.pizzaId) {
+      isPizzaFound = true;
+      break;
+    }
+  }
+
+  if (isPizzaFound) {
+    // Pizza found
+    var pizza = req.body;
+
+    //
+    // Parameter validation
+    //
+    if (pizza.hasOwnProperty('id') &&
+        pizza.hasOwnProperty('name') &&
+        pizza.hasOwnProperty('size') &&
+        pizza.hasOwnProperty('price')) {
+          pizzaList[index] = pizza;
+          res.status(204).send('Update Okey');
+    } else {
+      // parameter missing
+      res.status(400).send('Invalid pizza supplied');
+    }
   } else {
     // No pizza found
     res.status(404).send('Pizza could not be found');
