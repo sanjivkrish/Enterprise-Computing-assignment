ec2679d1bee38c3b48eb85f96448c3761c7e3622
Sanjiv Krish <sanjiv436.k@gmail.com>
2017-11-10 12:31:17 +0100

    server: routes: pizza: Include 'getPizza' operation
    pizzaOrderList --> pizzaList
    
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 //
 // List of pizzas
 //
-var pizzaOrderList = [];
+var pizzaList = [];
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -25,16 +25,16 @@ router.post('/pizza', (req, res) => {
         var isValidPizza = true;
 
         // Check for duplicate entries
-        for (var i = 0; i < pizzaOrderList.length; i++) {
+        for (var i = 0; i < pizzaList.length; i++) {
           // Duplicate ID
-          if (pizzaOrderList[i].id == pizza.id) {
+          if (pizzaList[i].id == pizza.id) {
             isValidPizza = false;
             res.status(401).send('Pizza with same ID already exists');
             break;
           }
 
           // Duplicate Name
-          if (pizzaOrderList[i].name == pizza.name) {
+          if (pizzaList[i].name == pizza.name) {
             isValidPizza = false;
             res.status(401).send('Pizza with same Name already exists');
             break;
@@ -43,13 +43,25 @@ router.post('/pizza', (req, res) => {
 
         // Add pizza to an existing list
         if (isValidPizza) {
-          pizzaOrderList.push(pizza);
+          pizzaList.push(pizza);
           res.status(201).send('Created new pizza');
         }
 
   } else {
     // parameter missing
     res.status(400).send('Invalid input');
+  }
+});
+
+//
+// Get pizza list
+//
+router.get('/pizza', (req, res) => {
+  if (pizzaList.length === 0) {
+    // No pizzas
+    res.status(404).send('No pizzas exists');
+  } else {
+    res.status(200).send(pizzaList);
   }
 });
 
